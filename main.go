45e2aa8e83177c0acf64bb1b5855d062292bcb14
Dale Bandoni <dalebandoni.10@gmail.com>
2025-02-02 13:25:22 +0000

    created event model and methods

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/dalebandoni/booking-api/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +12,7 @@ func main() {
 	s := gin.Default()
 
 	s.GET("/events", getEvents)
+	s.POST("/events", createEvent)
 
 	if err := s.Run(":8080"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
@@ -18,5 +20,23 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "hello"})
+	e := models.GetAllEvents()
+	context.JSON(http.StatusOK, e)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created.", "event": event})
 }
